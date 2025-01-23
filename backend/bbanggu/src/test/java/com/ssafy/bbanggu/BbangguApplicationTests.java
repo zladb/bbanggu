63541b6320ba54d5224d590cb599d746bb46ed9f
김휘동 <rlagnlehd0417@gmail.com>
2025-01-23 10:35:23 +0900

    init: 백엔드 Spring 초기 세팅

---
+++ backend/bbanggu/src/test/java/com/ssafy/bbanggu/BbangguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.bbanggu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbangguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
