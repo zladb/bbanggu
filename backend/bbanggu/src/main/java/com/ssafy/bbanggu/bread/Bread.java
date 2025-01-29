3e91fcd528a272c1f32b51d697fd156ce4afc666
김휘동 <rlagnlehd0417@gmail.com>
2025-01-29 19:35:55 +0900

    feat: 빵 정보 조회기능(개별) 추가

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/bread/Bread.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/bread/Bread.java
@@ -2,6 +2,8 @@ package com.ssafy.bbanggu.bread;
 
 import java.time.LocalDateTime;
 
+import com.ssafy.bbanggu.bakery.Bakery;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,11 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @Entity
--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/bread/BreadController.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/bread/BreadController.java
@@ -3,9 +3,13 @@ package com.ssafy.bbanggu.bread;
 import java.io.IOException;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,16 +27,38 @@ public class BreadController {
 		this.breadService = breadService;
 	}
 
-	@PostMapping("")
-	public ResponseEntity<String> insertBread(@RequestPart("bread") String breadJson,
+	@PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<String> insertBread(@RequestPart("bread") String breadDtoJson,
 		@RequestPart(value = "image", required = false) MultipartFile file) {
 		try {
+			System.out.println("dto: " + breadDtoJson);
 			ObjectMapper objectMapper = new ObjectMapper();
-			Bread bread = objectMapper.readValue(breadJson, Bread.class);
-			Bread insertedBread = breadService.insertBread(bread, file);
+			BreadDTO breadDto = objectMapper.readValue(breadDtoJson, BreadDTO.class);
+			Bread insertedBread = breadService.insertBread(breadDto, file);
 			return ResponseEntity.ok("빵 등록 성공: ID = " + insertedBread.getBreadId());
 		} catch (IOException e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("빵 등록 실패");
+		}
+	}
+
+	@GetMapping("/{breadId}")
+	public ResponseEntity<?> getBread(@PathVariable("breadId") long breadId) {
+		try {
+			Bread bread = breadService.getBread(breadId);
+			if (bread != null) {
+				BreadDTO breadDto = new BreadDTO();
+				breadDto.setBreadCategoryId(bread.getBreadCategory().getBreadCategoryId());
+				breadDto.setName(bread.getName());
+				breadDto.setPrice(bread.getPrice());
+				breadDto.setBakeryId(bread.getBakery().getBakeryId());
+				String imageUrl = bread.getBreadImageUrl().replace("\\", "/");
+				breadDto.setBreadImageUrl(imageUrl);
+				return ResponseEntity.ok(breadDto);
+			} else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("빵 정보를 찾을 수 없습니다.");
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다.");
 		}
 	}
 
