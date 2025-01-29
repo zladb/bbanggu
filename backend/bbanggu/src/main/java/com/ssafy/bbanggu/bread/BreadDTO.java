d874173f897e57d1d854c82cf0428d5915cb6759
김휘동 <rlagnlehd0417@gmail.com>
2025-01-29 19:35:55 +0900

    feat: 빵 정보 조회기능(개별) 추가

---
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/bread/BreadDTO.java
@@ -0,0 +1,14 @@
+package com.ssafy.bbanggu.bread;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BreadDTO {
+	private Long bakeryId;
+	private Long breadCategoryId;
+	private String name;
+	private Integer price;
+	private String breadImageUrl;
+}
