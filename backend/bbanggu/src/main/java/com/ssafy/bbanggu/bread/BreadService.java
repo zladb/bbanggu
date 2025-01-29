4cd72a3204483e10d1e87278d46c8b86758daa07
김휘동 <rlagnlehd0417@gmail.com>
2025-01-29 19:35:55 +0900

    feat: 빵 정보 조회기능(개별) 추가

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/bread/BreadService.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/bread/BreadService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ssafy.bbanggu.bakery.Bakery;
 import com.ssafy.bbanggu.util.image.ImageService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,10 +19,32 @@ public class BreadService {
 	private final BreadRepository breadRepository;
 	private final ImageService imageService;
 
-	public Bread insertBread(Bread bread, MultipartFile file) throws IOException {
+	public Bread insertBread(BreadDTO breadDto, MultipartFile file) throws IOException {
+		Bread bread = new Bread();
+
+		// id 유효성 검사가 필요 없으면 아래 코드 적용(프록시 방식)
+		Bakery bakery = new Bakery();
+		bakery.setBakeryId(breadDto.getBakeryId());
+		BreadCategory breadCategory = new BreadCategory();
+		breadCategory.setBreadCategoryId(breadDto.getBreadCategoryId());
+
+		// id 유효성 검사가 필요하면 아래 코드 적용
+		// Bakery bakery = bakeryRepository.findById(breadDto.getBakeryId())
+		// 	.orElseThrow(() -> new IllegalArgumentException("해당 bakeryId가 존재하지 않습니다."));
+		// BreadCategory breadCategory = breadCategoryRepository.findById(breadDto.getBreadCategoryId())
+		// 	.orElseThrow(() -> new IllegalArgumentException("해당 bakeryId가 존재하지 않습니다."));
+
+		bread.setBakery(bakery);
+		bread.setBreadCategory(breadCategory);
+		bread.setName(breadDto.getName());
+		bread.setPrice(breadDto.getPrice());
 		bread.setCreatedAt(LocalDateTime.now());
 		bread.setBreadImageUrl(imageService.saveImage(file));
 
 		return breadRepository.save(bread);
+	}
+
+	public Bread getBread(long breadId) {
+		return breadRepository.findById(breadId).orElse(null);
 	}
 }
