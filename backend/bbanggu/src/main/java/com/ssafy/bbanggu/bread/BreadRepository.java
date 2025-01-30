e9a97233c764e4e738e6fcdf6c332e9fb64c2695
김휘동 <rlagnlehd0417@gmail.com>
2025-01-30 16:52:13 +0900

    feat: 빵 정보 수정, 삭제, 전제조회기능 추가

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/bread/BreadRepository.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/bread/BreadRepository.java
@@ -1,6 +1,10 @@
 package com.ssafy.bbanggu.bread;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BreadRepository extends JpaRepository<Bread, Long> {
+	List<Bread> findByBakery_BakeryIdAndDeletedAtIsNull(long bakeryId);
+
 }
