73c6641353dcb2e085d73c9601c504da27178320
김휘동 <rlagnlehd0417@gmail.com>
2025-01-31 17:41:27 +0900

    재고 등록, 수정, 삭제기능 추가

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/stock/StockController.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/stock/StockController.java
@@ -4,17 +4,14 @@ import java.io.IOException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.bbanggu.bread.Bread;
 import com.ssafy.bbanggu.bread.BreadDTO;
 
 import lombok.RequiredArgsConstructor;
+import org.webjars.NotFoundException;
 
 @RestController
 @RequestMapping("/stock")
@@ -24,14 +21,29 @@ public class StockController {
 
 	// 재고 등록
 	@PostMapping("")
-	public ResponseEntity<String> insertStock(@RequestParam("stock") String stockDtoJson) {
+	public ResponseEntity<String> insertStock(@RequestBody StockDTO stockDto) {
+		Stock insertedStock = stockService.insertStock(stockDto);
+		return ResponseEntity.ok("재고 등록 성공: ID = " + insertedStock.getStockId());
+	}
+
+	@PutMapping("")
+	public ResponseEntity<String> updateStock(@RequestBody StockDTO stockDto) {
+		Stock updatedStock = stockService.updateStock(stockDto);
+		return ResponseEntity.ok("재고 수정 성공: ID = " + updatedStock.getStockId());
+	}
+
+	@GetMapping("/{stockId}")
+	public ResponseEntity<?> getStock(@PathVariable("stockId") long stockId) {
 		try {
-			ObjectMapper objectMapper = new ObjectMapper();
-			StockDTO breadDto = objectMapper.readValue(stockDtoJson, StockDTO.class);
-			Bread insertedBread = stockService.insertStock();
-			return ResponseEntity.ok("재고 등록 성공: ID = " + insertedBread.getBreadId());
-		} catch (IOException e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("재고 등록 실패");
+			StockDTO stockDTO = stockService.getStock(stockId);
+			return ResponseEntity.ok(stockDTO);
+		} catch (NotFoundException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("재고 정보를 찾을 수 없습니다.");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다.");
 		}
 	}
+
+
+
 }
