6943a4a14eaaab311cad07c2dd41e0e57c14c61f
서유민 <senna077@gmail.com>
2025-01-31 18:00:44 +0900

    메인화면 상세화면 연결

--- frontend/BBANGGU/src/components/user/bakerydetail/BakeryLocation.tsx
+++ frontend/BBANGGU/src/components/user/bakerydetail/BakeryLocation.tsx
@@ -1,5 +1,5 @@
 import { ChevronDown } from "lucide-react"
-import type { LocationType } from "../../types/bakery"
+import type { LocationType } from "../../../types/bakery"
 
 interface BakeryLocationProps {
   location: LocationType
@@ -11,11 +11,12 @@ export default function BakeryLocation({ location }: BakeryLocationProps) {
       <button className="flex w-full justify-between items-center px-[20px] gap-3">
         <div className="flex gap-3 items-center">
           <h2 className="whitespace-nowrap text-[14px] font-medium text-[#333333]">위치안내</h2>
-          <p className="text-[12px] font-light text-[#BDBDBD]">{location.address}</p>
+          <p className="text-[12px] font-light text-[#BDBDBD]">{location.address_road}</p>
         </div>
         <ChevronDown className="h-6 w-6 text-[#A2A2A2]" />
       </button>
     </div>
   )
 }
+
 
