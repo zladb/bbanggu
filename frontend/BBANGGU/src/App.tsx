10e52992f44583ab8cc5461831c73db56b99b11c
신은찬 <dmscks3126@gmail.com>
2025-02-02 15:51:50 +0900

    design & feat: 회원정보 페이지 구현

--- frontend/BBANGGU/src/App.tsx
+++ frontend/BBANGGU/src/App.tsx
@@ -1,8 +1,7 @@
-import React from "react"
 import UserMain from "./pages/user/UserMain"
 import BakeryDetail from "./pages/user/BakeryDetail"
 import "./styles/fonts.css"
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MobileLayout from './layouts/MobileLayout';
 import EditProfile from './pages/owner/profile/EditProfile';
 import EditSettlement from './pages/owner/settlement/EditSettlement';
@@ -10,24 +9,26 @@ import MyPage from './pages/owner/mypage/MyPage';
 import EditStore from './pages/owner/store/EditStore';
 import PickupTime from './pages/owner/pickup/PickupTime';
 import CustomerSupport from './pages/owner/support/CustomerSupport';
+import { ProfileProvider } from './common/context/ProfileContext';
 
 function App() {
   return (
-    <Router>
-      <MobileLayout>
-
-        <Routes>
-          <Route path="/owner/profile/edit" element={<EditProfile />} />
-          <Route path="/owner/settlement/edit" element={<EditSettlement />} />
-          <Route path="/owner/mypage" element={<MyPage />} />
-          <Route path="/owner/store/edit" element={<EditStore />} />
-          <Route path="/owner/pickup-time" element={<PickupTime />} />
-          <Route path="/owner/chatbot" element={<CustomerSupport />} />
-          <Route path="/" element={<UserMain />} />
-          <Route path="/bakery/:bakery_id" element={<BakeryDetail />} />
-        </Routes>
-      </MobileLayout>
-    </Router>
+    <ProfileProvider>
+      <Router>
+        <MobileLayout>
+          <Routes>
+            <Route path="/owner/profile/edit" element={<EditProfile />} />
+            <Route path="/owner/settlement/edit" element={<EditSettlement />} />
+            <Route path="/owner/mypage" element={<MyPage />} />
+            <Route path="/owner/store/edit" element={<EditStore />} />
+            <Route path="/owner/pickup-time" element={<PickupTime />} />
+            <Route path="/owner/chatbot" element={<CustomerSupport />} />
+            <Route path="/" element={<UserMain />} />
+            <Route path="/bakery/:bakery_id" element={<BakeryDetail />} />
+          </Routes>
+        </MobileLayout>
+      </Router>
+    </ProfileProvider>
   );
 }
 
