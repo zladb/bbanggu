772ca27a7917c674bc797dfafac2bf96511834ba
김휘동 <rlagnlehd0417@gmail.com>
2025-02-03 21:24:32 +0900

    feat: 재고 수정기능 추가

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/stock/StockController.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/stock/StockController.java
@@ -3,12 +3,17 @@ package com.ssafy.bbanggu.stock;
 import java.io.IOException;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.bbanggu.bread.Bread;
@@ -24,14 +29,24 @@ public class StockController {
 
 	// 재고 등록
 	@PostMapping("")
-	public ResponseEntity<String> insertStock(@RequestParam("stock") String stockDtoJson) {
+	public ResponseEntity<?> insertStock(@RequestBody StockDTO stockDto) {
 		try {
-			ObjectMapper objectMapper = new ObjectMapper();
-			StockDTO breadDto = objectMapper.readValue(stockDtoJson, StockDTO.class);
-			Bread insertedBread = stockService.insertStock();
-			return ResponseEntity.ok("재고 등록 성공: ID = " + insertedBread.getBreadId());
+			Stock insertedStock = stockService.insertStock();
+			return null;
 		} catch (IOException e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("재고 등록 실패");
+		}
+	}
+
+	@PutMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<String> updateStock(@RequestBody StockDTO stockDto) {
+		try {
+			System.out.println("dto: " + stockDto);
+			stockService.updateStock(stockDto);
+
+			return ResponseEntity.ok("빵 수정 성공: ID = " + insertedBread.getBreadId());
+		} catch (IOException e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("빵 등록 실패");
 		}
 	}
 }
