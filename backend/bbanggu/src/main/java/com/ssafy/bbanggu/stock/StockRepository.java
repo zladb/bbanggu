f60292925300bf4e3b19a180d1a2feed62b7b509
김휘동 <rlagnlehd0417@gmail.com>
2025-02-04 16:03:35 +0900

    재고 조회 기능 추가

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/stock/StockRepository.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/stock/StockRepository.java
@@ -2,5 +2,9 @@ package com.ssafy.bbanggu.stock;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface StockRepository extends JpaRepository<Stock, Long> {
+	List<Stock> findByBakery_BakeryIdAndDateBetween(long bakeryId, LocalDate startDate, LocalDate endDate);
 }
