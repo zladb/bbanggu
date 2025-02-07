ce1ab9fe4df49a7851bca4dbaac7288c73f86303
신은찬 <dmscks3126@gmail.com>
2025-02-07 15:06:30 +0900

    feat: 지도 오류 수정

--- frontend/BBANGGU/src/main.tsx
+++ frontend/BBANGGU/src/main.tsx
@@ -9,9 +9,27 @@ declare global {
   }
 }
 
-// React 렌더링만 수행
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const loadKakaoMapScript = () => {
+  return new Promise<void>((resolve, reject) => {
+    const script = document.createElement('script');
+    script.id = 'kakao-map-script';
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_MAP_API_KEY}&libraries=services,clusterer&autoload=false`;
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        resolve();
+      });
+    };
+    script.onerror = (e) => reject(e);
+    document.head.appendChild(script);
+  });
+};
+
+loadKakaoMapScript()
+  .then(() => {
+    ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    )
+  })
+  .catch((e) => console.error('카카오맵 초기화 중 오류 발생:', e));
