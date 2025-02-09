7d132487e4043f958e874957b710f73019793b54
ì Nageum Jeong <nagum0545@naver.com>
2025-02-09 21:12:22 +0900

    feat: 회원가입 시 echo 데이터 자동 생성 및 초기화 구현

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/saving/domain/EchoSaving.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/saving/domain/EchoSaving.java
@@ -4,15 +4,21 @@ import com.ssafy.bbanggu.user.domain.User;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 /**
  * 유저의 탄소 절감량 및 절약 금액을 저장하는 엔티티
  */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 @Table(name = "echo_saving")
+@Builder
 public class EchoSaving {
 
 	@Id
@@ -30,22 +36,21 @@ public class EchoSaving {
 	@Column(name = "reduced_co2e", nullable = false)
 	private int reducedCo2e;
 
-	@Column(name = "created_at", nullable = false, updatable = false)
-	private LocalDateTime createdAt = LocalDateTime.now();
+	@Column(name = "created_at", updatable = false)
+	@CreationTimestamp
+	private LocalDateTime createdAt;
 
-	@Column(name = "updated_at", nullable = false)
-	private LocalDateTime updatedAt = LocalDateTime.now();
+	@Column(name = "updated_at")
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
 
-	@Builder
-	public EchoSaving(User user, int savedMoney, int reducedCo2e) {
-		this.user = user;
-		this.savedMoney = savedMoney;
-		this.reducedCo2e = reducedCo2e;
-	}
-
-	public void update(int savedMoney, int reducedCo2e) {
-		this.savedMoney += savedMoney;
+	/**
+	 * 절약 정보 업데이트
+	 * @param reducedCo2e 추가된 Co2 절감량
+	 * @param savedMoney 추가된 절약 금액
+	 */
+	public void updateSaving(int reducedCo2e, int savedMoney) {
 		this.reducedCo2e += reducedCo2e;
-		this.updatedAt = LocalDateTime.now();
+		this.savedMoney += savedMoney;
 	}
 }
