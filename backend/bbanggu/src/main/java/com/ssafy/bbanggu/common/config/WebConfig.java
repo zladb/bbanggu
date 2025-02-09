8d5c2f2e91c74193936ce6ab5b116d69c9403aba
Yujin KIM <zla_db@naver.com>
2025-02-09 21:39:17 +0900

    fix: WebConfig에 addCorsMappings localhost:5173 허용해서 추가

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/common/config/WebConfig.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/common/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOriginPatterns("*") // “*“같은 와일드카드를 사용
+			.allowedOrigins("http://localhost:5173")  // 명시적으로 localhost:5173 허용
 			.allowedMethods("GET", "POST") // 허용할 HTTP method
 			.allowCredentials(true); // 쿠키 인증 요청 허용
 	}
