0eea2ded47fb3ebccb842ebe3802d5b6bb6cf0ee
Yujin KIM <zla_db@naver.com>
2025-02-09 18:26:13 +0900

    chore: CORS 에러 해결을 위한 webconfig 파일 수정

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/common/config/WebConfig.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/common/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.bbanggu.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/uploads/**")
 			.addResourceLocations("file:uploads/"); // 상대 경로 사용
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns("*") // “*“같은 와일드카드를 사용
+			.allowedMethods("GET", "POST") // 허용할 HTTP method
+			.allowCredentials(true); // 쿠키 인증 요청 허용
 	}
 }
