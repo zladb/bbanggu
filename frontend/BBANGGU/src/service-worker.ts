336bb3d25ee5ef3945047e955a00abcaafa89106
신은찬 <dmscks3126@gmail.com>
2025-02-13 15:25:17 +0900

    PWA 1차준비 (npm run build 오류 수정)

--- frontend/BBANGGU/src/service-worker.ts
+++ frontend/BBANGGU/src/service-worker.ts
@@ -1,8 +1,9 @@
 /// <reference lib="webworker" />
+declare const self: ServiceWorkerGlobalScope;
 
 const CACHE_NAME = 'bbanggu-cache-v1';
 
-self.addEventListener('install', (event: ExtendableEvent) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
@@ -15,7 +16,7 @@ self.addEventListener('install', (event: ExtendableEvent) => {
   );
 });
 
-self.addEventListener('fetch', (event: FetchEvent) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
