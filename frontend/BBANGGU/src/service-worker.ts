62f79b1ee9e019d99c61f53b667d5be6fe9e4f54
신은찬 <dmscks3126@gmail.com>
2025-02-13 15:22:10 +0900

    feat: PWA 1차 준비

---
+++ frontend/BBANGGU/src/service-worker.ts
@@ -0,0 +1,24 @@
+/// <reference lib="webworker" />
+
+const CACHE_NAME = 'bbanggu-cache-v1';
+
+self.addEventListener('install', (event: ExtendableEvent) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        // 필요한 정적 자원들 추가
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event: FetchEvent) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+}); 
