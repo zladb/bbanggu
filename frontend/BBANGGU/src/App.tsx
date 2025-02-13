54a369e8ac7cb04b1dee47431b22da55f35ec237
신은찬 <dmscks3126@gmail.com>
2025-02-14 01:22:38 +0900

    refactor: 불필요한 테스트 페이지 route 제거

--- frontend/BBANGGU/src/App.tsx
+++ frontend/BBANGGU/src/App.tsx
@@ -38,7 +38,6 @@ import PackageSalesSetting from './pages/owner/package/PackageSalesSetting';
 import PackageSettingPage from './pages/owner/package/PackageSettingPage';
 import BreadRegisterPage from './pages/owner/bread/BreadRegisterPage';
 import { InstallPWA } from './components/InstallPWA';
-import CameraTest from './pages/test/CameraTest';
 import PackageCameraTest from './pages/owner/package/PackageCameraTest';
 
 
@@ -91,7 +90,6 @@ export default function App() {
               <Route path="/owner/package/sales-setting" element={<PackageSalesSetting />} />
               <Route path="/owner/package/setting" element={<PackageSettingPage />} />
               <Route path="/owner/bread/register" element={<BreadRegisterPage />} />
-              <Route path="/test/camera" element={<CameraTest />} />
               <Route path="/owner/package/camera-test" element={<PackageCameraTest />} />
             </Routes>
             <InstallPWA />
