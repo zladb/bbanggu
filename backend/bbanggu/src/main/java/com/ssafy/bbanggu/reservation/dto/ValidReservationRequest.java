7ad93fe9c63843fdd9ef88f48a64c343794b89e4
ì Nageum Jeong <nagum0545@naver.com>
2025-02-13 22:47:52 +0900

    refactor: 예약 취소 및 환불 api 최종 수정

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/reservation/dto/ValidReservationRequest.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/reservation/dto/ValidReservationRequest.java
@@ -2,7 +2,7 @@ package com.ssafy.bbanggu.reservation.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-public record checkQuantityRequest(
+public record ValidReservationRequest(
 	@NotNull(message = "필수 필드인 '빵꾸러미 ID'가 없습니다.")
 	Long breadPackageId,
 	@NotNull(message = "필수 필드인 '빵꾸러미 개수'가 없습니다.")
