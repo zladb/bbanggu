7fe8f7aa2b1c62562df1ad3f485aecb31f4c237c
ì Nageum Jeong <nagum0545@naver.com>
2025-02-14 14:34:26 +0900

    fix: CORS 에러 수정

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/common/config/CustomCorsFilter.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/common/config/CustomCorsFilter.java
@@ -17,7 +17,7 @@ public class CustomCorsFilter {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
 		config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
 
 		source.registerCorsConfiguration("/**", config);
