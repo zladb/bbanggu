f1ef80d2564dd8da092625d1d8d36e32c70caeda
ì Nageum Jeong <nagum0545@naver.com>
2025-02-14 12:02:41 +0900

    refactor: 가게 정보 응답에 좋아요, 픽업시간 추가 및 토큰 반환 수정

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/favorite/FavoriteService.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/favorite/FavoriteService.java
@@ -1,6 +1,7 @@
 package com.ssafy.bbanggu.favorite;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -8,10 +9,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.bbanggu.auth.security.CustomUserDetails;
 import com.ssafy.bbanggu.bakery.domain.Bakery;
 import com.ssafy.bbanggu.bakery.dto.BakeryDetailDto;
+import com.ssafy.bbanggu.bakery.dto.PickupTimeDto;
 import com.ssafy.bbanggu.bakery.repository.BakeryRepository;
+import com.ssafy.bbanggu.bakery.service.BakeryPickupService;
 import com.ssafy.bbanggu.bakery.service.BakeryService;
+import com.ssafy.bbanggu.breadpackage.BreadPackage;
+import com.ssafy.bbanggu.breadpackage.BreadPackageService;
+import com.ssafy.bbanggu.breadpackage.dto.BreadPackageDto;
 import com.ssafy.bbanggu.common.exception.CustomException;
 import com.ssafy.bbanggu.common.exception.ErrorCode;
 import com.ssafy.bbanggu.user.domain.User;
@@ -27,6 +34,8 @@ public class FavoriteService {
 	private final BakeryService bakeryService;
 	private final UserRepository userRepository;
 	private final BakeryRepository bakeryRepository;
+	private final BakeryPickupService bakeryPickupService;
+	private final BreadPackageService breadPackageService;
 
 	// 좋아요 추가
 	@Transactional
@@ -66,9 +75,20 @@ public class FavoriteService {
 
 	// 유저가 좋아요한 모든 가게 조회
 	@Transactional(readOnly = true)
-	public Page<BakeryDetailDto> findAllFavorites(Long userId, Double userLat, Double userLng, Pageable pageable) {
+	public Page<BakeryDetailDto> findAllFavorites(CustomUserDetails userDetails, Pageable pageable) {
+		// 사용자 위치 정보 추출
+		final Double userLat = Optional.ofNullable(userDetails)
+			.filter(u -> u.getLatitude() != 0.0)
+			.map(CustomUserDetails::getLatitude)
+			.orElse(null);
+
+		final Double userLng = Optional.ofNullable(userDetails)
+			.filter(u -> u.getLongitude() != 0.0)
+			.map(CustomUserDetails::getLongitude)
+			.orElse(null);
+
 		// 좋아요 누른 가게 아이디 조회
-		List<Favorite> favorites = favoriteRepository.findByUser_UserId(userId);
+		List<Favorite> favorites = favoriteRepository.findByUser_UserId(userDetails.getUserId());
 		List<Long> bakeryIds = favorites.stream()
 			.map(f -> f.getBakery().getBakeryId())
 			.toList();
@@ -82,7 +102,14 @@ public class FavoriteService {
 			.map(b -> {
 				double distance = (userLat == null || userLng == null) ? 0.0
 					: bakeryService.calculateDistance(userLat, userLng, b.getLatitude(), b.getLongitude());
-				return BakeryDetailDto.from(b, distance);
+				boolean is_liked = favoriteRepository.existsByUser_UserIdAndBakery_BakeryId(userDetails.getUserId(), b.getBakeryId());
+				PickupTimeDto pickupTime = bakeryPickupService.getPickupTimetable(b.getBakeryId());
+				BreadPackage breadPackage = breadPackageService.getPackageById(b.getBakeryId());
+				int price = 0;
+				if (breadPackage != null) {
+					price = breadPackage.getPrice();
+				}
+				return BakeryDetailDto.from(b, distance, is_liked, pickupTime, price);
 			})
 			.toList();
 
@@ -90,18 +117,47 @@ public class FavoriteService {
 	}
 
 	@Transactional(readOnly = true)
-	public List<BakeryDetailDto> getTop10BestBakeries(Double userLat, Double userLng) {
+	public List<BakeryDetailDto> getTop10BestBakeries(CustomUserDetails userDetails) {
+		// 사용자 위치 정보 추출
+		final Double userLat = Optional.ofNullable(userDetails)
+			.filter(u -> u.getLatitude() != 0.0)
+			.map(CustomUserDetails::getLatitude)
+			.orElse(null);
+
+		final Double userLng = Optional.ofNullable(userDetails)
+			.filter(u -> u.getLongitude() != 0.0)
+			.map(CustomUserDetails::getLongitude)
+			.orElse(null);
+
 		if (userLat == null || userLng == null) {
 			return bakeryRepository.findTop10ByFavorites().stream()
-				.map(bakery -> BakeryDetailDto.from(bakery, 0.0)) // ✅ 로그인X or 주소 미등록 → 거리 0.0km 처리
+				.map(b -> {
+					double distance = (userLat == null || userLng == null) ? 0.0
+						: bakeryService.calculateDistance(userLat, userLng, b.getLatitude(), b.getLongitude());
+					boolean is_liked = favoriteRepository.existsByUser_UserIdAndBakery_BakeryId(userDetails.getUserId(), b.getBakeryId());
+					PickupTimeDto pickupTime = bakeryPickupService.getPickupTimetable(b.getBakeryId());
+					BreadPackage breadPackage = breadPackageService.getPackageById(b.getBakeryId());
+					int price = 0;
+					if (breadPackage != null) {
+						price = breadPackage.getPrice();
+					}
+					return BakeryDetailDto.from(b, distance, is_liked, pickupTime, price);
+				}) // ✅ 로그인X or 주소 미등록 → 거리 0.0km 처리
 				.toList();
 		}
 
 		return bakeryRepository.findBestBakeriesByLocation(userLat, userLng).stream()
-			.map(bakery -> {
-				double distance = bakeryService.calculateDistance(userLat, userLng, bakery.getLatitude(),
-					bakery.getLongitude());
-				return BakeryDetailDto.from(bakery, distance);
+			.map(b -> {
+				double distance = (userLat == null || userLng == null) ? 0.0
+					: bakeryService.calculateDistance(userLat, userLng, b.getLatitude(), b.getLongitude());
+				boolean is_liked = favoriteRepository.existsByUser_UserIdAndBakery_BakeryId(userDetails.getUserId(), b.getBakeryId());
+				PickupTimeDto pickupTime = bakeryPickupService.getPickupTimetable(b.getBakeryId());
+				BreadPackage breadPackage = breadPackageService.getPackageById(b.getBakeryId());
+				int price = 0;
+				if (breadPackage != null) {
+					price = breadPackage.getPrice();
+				}
+				return BakeryDetailDto.from(b, distance, is_liked, pickupTime, price);
 			})
 			.toList();
 	}
