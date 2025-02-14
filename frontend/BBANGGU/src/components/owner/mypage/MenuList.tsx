4321286f296a8459bd35ef577123d9c7a18787c8
김유진 <zla_db@naver.com>
2025-02-15 02:36:28 +0900

    fix: build 에러 수정

--- frontend/BBANGGU/src/components/owner/mypage/MenuList.tsx
+++ frontend/BBANGGU/src/components/owner/mypage/MenuList.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import pickupTimeIcon from '@/assets/icons/pickup-time-icon.svg';
 import settlementIcon from '@/assets/icons/settlement-icon.svg';
