7c980993a747a88ebff23c1c222d979f9e718b80
김휘동 <rlagnlehd0417@gmail.com>
2025-02-14 19:32:23 +0900

    feat: efficientNet 추가

--- bbanggu_ai/main.py
+++ bbanggu_ai/main.py
@@ -2,11 +2,16 @@
 from fastapi import FastAPI, UploadFile, File, Form
 from typing import List
 from pathlib import Path
-from ai.detect import detect_breads_from_image
-from ai import auto_labeling
+from ai import detect, detect_crop, auto_labeling
 from collections import defaultdict
 import shutil
 import os
+import cv2
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.models as models
+from PIL import Image
 
 app = FastAPI()
 
@@ -16,10 +21,47 @@ async def detect_breads(images: List[UploadFile]):
     result_list = defaultdict(int)
     for image in images:
         image_bytes = await image.read()
-        result = detect_breads_from_image(image_bytes)
+        result = detect.detect_breads_from_image(image_bytes)
         for class_name, count in result.items():
             result_list[class_name] += count
     return result_list
+
+
+def load_model(model_path, num_classes):
+    model = models.efficientnet_b2(weights=True)
+    model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.eval()
+    return model
+
+
+@app.post("/detectcrop")
+async def detect_breads(images: List[UploadFile]):
+    for image in images:
+        image_bytes = await image.read()
+        detect_crop.detect_and_crop(image_bytes)
+    model_path = './models/efficientnet_b2.pth'  # 학습된 모델 경로
+    cropped_image_dir = 'cropped_objects'  # YOLO로 크롭된 이미지 폴더
+
+    class_names = [
+        'bagel', 'baguette', 'bun', 'cake', 'croissant', 'croquette',
+        'financier', 'pizza', 'pretzel', 'red_bean', 'scone', 'soboro', 'tart', 'white_bread'
+    ]
+    model = load_model(model_path, num_classes=len(class_names))
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    for img_name in os.listdir(cropped_image_dir):
+        img_path = os.path.join(cropped_image_dir, img_name)
+        image = Image.open(img_path).convert('RGB')
+        image = transform(image).unsqueeze(0)  # 배치 차원 추가
+
+        with torch.no_grad():
+            output = model(image)
+            predicted_class = torch.argmax(output, dim=1).item()
+            print(f"{img_name}: Predicted as {class_names[predicted_class]}")
 
 
 UPLOAD_DIR = "uploads"
