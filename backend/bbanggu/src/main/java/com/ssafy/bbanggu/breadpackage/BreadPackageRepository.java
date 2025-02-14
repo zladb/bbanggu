6e9f9c23c7c44a89b16b986ae3b34b2221733a4e
ì Nageum Jeong <nagum0545@naver.com>
2025-02-14 12:10:02 +0900

    refactor: 가게 정보 응답에 좋아요, 픽업시간 추가 및 토큰 반환 수정

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/breadpackage/BreadPackageRepository.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/breadpackage/BreadPackageRepository.java
@@ -1,6 +1,8 @@
 package com.ssafy.bbanggu.breadpackage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,6 +17,10 @@ public interface BreadPackageRepository extends JpaRepository<BreadPackage, Long
 	List<BreadPackage> findByBakery_BakeryIdAndDeletedAtIsNull(Long bakeryId);
 
 	// BreadPackageRepository
+	// ✅ 오늘 날짜의 빵꾸러미만 조회
+	@Query("SELECT b FROM BreadPackage b WHERE b.bakery.bakeryId = :bakeryId AND DATE(b.createdAt) = CURRENT_DATE AND b.deletedAt IS NULL")
+	Optional<BreadPackage> findByBakeryIdAndToday(@Param("bakeryId") Long bakeryId);
+
 	List<BreadPackage> findByBakery_BakeryIdAndCreatedAtBetweenAndDeletedAtIsNull(Long bakeryId, LocalDateTime startDate, LocalDateTime endDate);
 
 }
