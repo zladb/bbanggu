a31d03d1a6febd750f06f54de7e1c895d0a6de2c
신은찬 <dmscks3126@gmail.com>
2025-02-14 09:50:40 +0900

    feat: 카메라 내장 카메라로 변환

--- frontend/BBANGGU/src/pages/owner/package/PackageAnalysis.tsx
+++ frontend/BBANGGU/src/pages/owner/package/PackageAnalysis.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../../components/owner/header/Header';
 
