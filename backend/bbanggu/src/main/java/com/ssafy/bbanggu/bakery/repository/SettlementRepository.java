07923e08d44d8c71f05f30595403e2de05c58000
ì Nageum Jeong <nagum0545@naver.com>
2025-02-14 22:09:03 +0900

    refactor: bakery, settlement, user 수정

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/bakery/repository/SettlementRepository.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/bakery/repository/SettlementRepository.java
@@ -1,11 +1,13 @@
 package com.ssafy.bbanggu.bakery.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.bbanggu.bakery.domain.Settlement;
 
 @Repository
-public interface BakerySettlementRepository extends JpaRepository<Settlement, Long> {
-
+public interface SettlementRepository extends JpaRepository<Settlement, Long> {
+	Optional<Settlement> findByBakery_BakeryId(Long bakeryId);
 }
