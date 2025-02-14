53e2135f4c84e55fdfb656e3c580edcb10bb3540
Yujin KIM <zla_db@naver.com>
2025-02-14 16:02:06 +0900

    feat: 점주 회원 정보 수정 (비밀번호 제외)

--- frontend/BBANGGU/src/pages/owner/profile/EditProfile.tsx
+++ frontend/BBANGGU/src/pages/owner/profile/EditProfile.tsx
@@ -1,12 +1,19 @@
+import { useLocation } from 'react-router-dom';
 import { Header } from '../../../components/owner/editprofile/Header';
 import { ProfileForm } from '../../../components/owner/editprofile/ProfileForm';
 import BottomNavigation from '../../../components/owner/navigations/BottomNavigations/BottomNavigation';
+import { ProfileSectionProps } from '../../../types/owner';
+
+type UserInfo = ProfileSectionProps['userInfo'];
 
 function EditProfile() {
+  const location = useLocation();
+  const userInfo = location.state?.userInfo as UserInfo;
+
   return (
     <div className="min-h-screen bg-white pb-20">
       <Header title="회원정보수정" />
-      <ProfileForm />
+      <ProfileForm userInfo={userInfo} />
       <BottomNavigation />
     </div>
   );
