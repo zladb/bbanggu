c8c3a0608766b6bed200336da101745c0e689bbf
김휘동 <rlagnlehd0417@gmail.com>
2025-02-15 17:24:29 +0900

    feat: 빵 조합 알고리즘 구현

--- bbanggu_ai/main.py
+++ bbanggu_ai/main.py
@@ -4,9 +4,10 @@ import uuid
 from collections import defaultdict
 from typing import List
 
-from fastapi import FastAPI, UploadFile
+import httpx
+from fastapi import FastAPI, UploadFile, File, Form
 
-from ai import yolo, efficientnet
+from ai import yolo, efficientnet, pacakge_maker
 
 app = FastAPI()
 
@@ -23,7 +24,7 @@ async def detect_breads(images: List[UploadFile]):
 
 
 @app.post("/detectcrop")
-async def detect_breads(images: List[UploadFile], bakeryId: int):
+async def detect_breads(images: List[UploadFile] = File(...), bakeryId: int = Form(...)):
     # 크롭된 이미지 저장할 폴더 생성(요청마다 다른 폴더 생성함)
     unique_id = str(uuid.uuid4())
     cropped_image_dir = os.path.join("cropped_objects", unique_id)
@@ -33,8 +34,29 @@ async def detect_breads(images: List[UploadFile], bakeryId: int):
         image_bytes = await image.read()
         yolo.detect_and_crop(image_bytes, cropped_image_dir)
 
-    # efficientNet으로 분류
-    class_filter = ['bagel', 'pizza', 'tart']
-    classified_breads = efficientnet.classify(cropped_image_dir, class_filter)
+    # 빵 분류 및 조합
+    class_names = [
+        'bagel', 'baguette', 'bun', 'cake', 'croissant', 'croquette',
+        'financier', 'pizza', 'pretzel', 'red_bean', 'scone', 'soboro', 'tart', 'white_bread'
+    ]
+    # 가게에 등록된 빵 정보 불러오기
+    class_filter = []
+    category_infos = {}
+    # SPRING_SERVER_URL = "http://bbanggu-server:8081/"
+    SPRING_SERVER_URL = "https://i12d102.p.ssafy.io/api"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{SPRING_SERVER_URL}/bread/bakery/{bakeryId}")
+        bakery_breads = response.json()
+        for bread in bakery_breads:
+            category_id = bread['breadCategoryId']
+            category_name = class_names[int(category_id) - 1] # id가 1부터 시작해서 1 뺌
+            class_filter.append(category_name)
+            category_infos[category_name] = bread['price']
 
-    return classified_breads
+    print(category_infos)
+
+    # efficientNet으로 분류
+    classified_breads = efficientnet.classify(cropped_image_dir, class_filter)
+    # 빵 조합 생성
+    result = pacakge_maker.distribute_breads(classified_breads, category_infos, class_names)
+    return result, classified_breads
