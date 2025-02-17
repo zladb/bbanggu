786ecc04037980508e1ce978d8913187908d3f7d
서유민 <senna077@gmail.com>
2025-02-18 02:24:23 +0900

    좋아요 errorfix

---
+++ frontend/BBANGGU/src/store/slices/reviewSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export interface ReviewType {
+    reviewId: number;
+    reservationId: number;
+    userId: number;
+    bakeryId: number;
+    rating: number;
+    comment: string;
+}
+
+export interface ReviewState {
+    reviews: ReviewType[];
+    loading: boolean;
+    error: string | null;
+}
+
+const initialState: ReviewState = {
+    reviews: [],
+    loading: false,
+    error: null
+}
+
+const reviewSlice = createSlice({
+    name: 'review',
+    initialState,
+    reducers: {
+        setReviews: (state, action) => {
+            state.reviews = action.payload;
+        },
+        setLoading: (state, action) => {
+            state.loading = action.payload;
+        },
+        setError: (state, action) => {
+            state.error = action.payload;
+        }
+    }
+})
+
+export default reviewSlice.reducer;
