0a9823febf36dabb08c6c8fa2eb0f64b1d1269e5
ì Nageum Jeong <nagum0545@naver.com>
2025-02-17 14:47:23 +0900

    refactor: user 디렉토리 청소

--- backend/bbanggu/src/main/java/com/ssafy/bbanggu/user/repository/UserRepository.java
+++ backend/bbanggu/src/main/java/com/ssafy/bbanggu/user/repository/UserRepository.java
@@ -21,29 +21,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	boolean existsByEmail(String email);
 
 	/**
-	 * 이메일과 비밀번호로 사용자 조회
-	 *
-	 * @param email 사용자 이메일
-	 * @param password 사용자 비밀번호
-	 * @return User 엔티티
-	 */
-	Optional<User> findByEmailAndPassword(String email, String password);
-
-	/**
 	 * 이메일로 사용자 조회
 	 *
 	 * @param email 사용자 이메일
 	 * @return User 엔티티
 	 */
 	Optional<User> findByEmail(String email);
-
-	/**
-	 * Refresh Token으로 사용자 조회
-	 *
-	 * @param refreshToken
-	 * @return
-	 */
-	Optional<User> findByRefreshToken(String refreshToken);
 
 	Optional<User> findByKakaoId(String kakaoId);
 
