5be17d06b7678fee9e5d5478dfef86eabca59587
Yujin KIM <zla_db@naver.com>
2025-02-18 13:18:53 +0900

    fix : build 에러 수정 (베스트 빵꾸러미 부분 주석처리)

--- frontend/BBANGGU/src/pages/user/main/UserMain.tsx
+++ frontend/BBANGGU/src/pages/user/main/UserMain.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
-import { fetchBestFavoriteStores, searchBakery } from "../../../services/user/usermainService"
+import { searchBakery } from "../../../services/user/usermainService"
+// import { fetchBestFavoriteStores } from "../../../services/user/usermainService"
 import SearchBar from "../../../components/user/usermain/SearchBar"
 import Header from "../../../components/user/usermain/Header"
 import BestPackages from "../../../components/user/usermain/BestPackages"
