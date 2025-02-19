52d6b14fb98abe212886790d7f8d7553928aade3
서유민 <senna077@gmail.com>
2025-02-19 14:10:32 +0900

    js

--- frontend/BBANGGU/dev-dist/sw.js
+++ frontend/BBANGGU/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.utgqrroeqi8"
+    "revision": "0.5odfbee09k8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
